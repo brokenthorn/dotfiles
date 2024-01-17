93e5ec74730172261d63349c282f2641e4aa1a0e
Paul-Sebastian Manole <brokenthorn@gmail.com>
2024-01-17 03:53:03 +0200

    update

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "aquarium",
-  theme_toggle = { "aquarium", "palenight" },
+  theme = "mountain",
+  theme_toggle = { "mountain", "palenight" },
   hl_override = highlights.override,
   hl_add = highlights.add,
 }
